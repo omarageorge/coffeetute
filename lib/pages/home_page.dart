d6705c9a995e8ae180128c86a2998cc8c0be28ec
George Omara <georgeomara8@gmail.com>
2023-05-31 13:11:37 +0300

    Added bottom navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:coffeetute/constants.dart';
+import 'package:coffeetute/components/bottom_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      bottomNavigationBar: const BottomNavBar(),
+    );
   }
 }
